6852ad7864f5258f3959bc7262e6a7a423c3ded4
XavZol <alansolis707@gmail.com>
2026-04-16 13:14:04 -0600

    Funciones de Nivel Inferior

--- Seaborn_New.py
+++ Seaborn_New.py
@@ -1,47 +1,13 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-propinas = sns.load_dataset("tips")
-
-sns.catplot(data=propinas,
-            kind="strip",
-            x="day",
-            y="total_bill",
-            hue="smoker");
+pinguinos = sns.load_dataset("penguins")
+sns.jointplot(data=pinguinos,
+                x="flipper_length_mm",
+                y="bill_length_mm",
+                hue="species")
 plt.show()
 
-sns.catplot(data=propinas,
-            kind="swarm", # evita el solapamiento
-            x="day",
-            y="total_bill",
-            hue="smoker");
+sns.pairplot(data=pinguinos,
+                hue="species",)
 plt.show()
-
-sns.catplot(data=propinas,
-            kind="box", # diagrama de caja y bigotes
-            x="day",
-            y="total_bill",
-            hue="smoker");
-plt.show()
-
-sns.catplot(data=propinas,
-            kind="violin", 
-            x="day",
-            y="total_bill",
-            hue="smoker");
-plt.show()
-
-sns.catplot(data=propinas,
-            kind="point", 
-            x="day",
-            y="total_bill",
-            hue="smoker");
-plt.show()
-
-sns.catplot(data=propinas,
-            kind="bar", 
-            x="day",
-            y="total_bill",
-            hue="smoker");
-plt.show()
-

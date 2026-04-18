f8160e3b494bdf4c2581d628d85fc887ca8986ca
XavZol <alansolis707@gmail.com>
2026-04-18 12:16:15 -0600

    Regreción lineal

--- Seaborn_New.py
+++ Seaborn_New.py
@@ -1,84 +1,28 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import linear_model
 
-diamantes = sns.load_dataset("diamonds")
+df = pd.DataFrame()
+df["Area"] = [2600, 3000, 3200, 3600, 4000]
+df["Precio"] = [550000, 565000, 610000, 680000, 725000]
 
-print(diamantes.head())
-
-print(diamantes.describe())
-
-sns.set_theme(style="whitegrid")
-
-g = sns.relplot(data=diamantes,
-                x="carat",
-                y="price",
-                kind="scatter",
-                height=6,
-                aspect=1.5)
-
-g.fig.suptitle('Relación entre el Precio y el Peso en Quilates de los Diamantes',
-                va='baseline',
-                ha='center')
-
-g.set_axis_labels('Peso en Quilates', 'Precio');
+plt.scatter(df["Area"],df["Precio"]);
 plt.show()
 
-# Creando la visualización de la distribución del precio
-g = sns.displot(data=diamantes,
-                x="price",
-                kind="hist",
-                kde=True,
-                color="blue",
-                height=4,
-                aspect=2)
-g.figure.suptitle('Distribución del Precio de los Diamantes',
-                va='baseline',
-                ha='center')
-g.set_axis_labels('Precio ($)', 'Frecuencia');
+X = df[["Area"]]
+print(type(df[["Area"]]))
+
+y = df["Precio"]
+
+modelo = linear_model.LinearRegression()
+modelo.fit(X, y)
+print(modelo.predict([[3300]]))
+
+# Relación entre las Areas y los precios
+plt.plot(df["Area"],modelo.predict(X));
 plt.show()
 
-# Creando la visualización de la distribución del peso en quilates
-g = sns.displot(data=diamantes,
-                x="carat",
-                kde=True,
-                color="green",
-                )
-g.set_titles('Distribución del Precio en Quilates')
-g.set(xlabel='Carat', ylabel='Frecuencia')
-plt.show();
-
-# Creando el gráfico catplot de tipo 'box'
-g = sns.catplot(data=diamantes,
-                kind="box",
-                x="cut",
-                y="price",
-                height=5,
-                aspect=2,
-                order=["Fair", "Good", "Very Good", "Premium", "Ideal"])
-# Establecido título
-g.figure.suptitle('Comparación del Precio de los Diamantes por Calidad del Corte',
-                    va='baseline',
-                    ha='center')
-# Establecido etiquetas de ejes
-g.set_axis_labels("Calidad del Corte",
-                    "Precio ($)");
-plt.show();
-
-# Utilizando joinplot para visualizar  la relación entre 'depth' y 'price' con un gráfico de densidad kernel
-g = sns.jointplot(data=diamantes,
-                    x="depth",
-                    y="price",
-                    kind="kde",
-                    fill=True,
-                    space=0,
-                    color="purple")
-# Cambiando las etiquetas de los ejes
-g.set_axis_labels("Profundidad del Diamante (%)",
-                    "Precio ($)")
-# Mejorando el título
-g.fig.suptitle('Relación entre la Profundidad y el Precio de los Diamantes',
-                va='baseline',
-                ha='center',
-                fontsize=16);
+plt.scatter(df["Area"], df["Precio"])
+plt.plot(df["Area"],modelo.predict(X));
 plt.show()
